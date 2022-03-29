ecaa9568429634e5a8b5b4de2be50abf06b4f908
TheMaksimys <99420487+TheMaksimys@users.noreply.github.com>
2022-03-29 16:32:51 +0100

    Update Assignment3.ino
    Formatting code, queue added, changed baudrate, optimisation and further finalization of the project

--- Assignment3.ino
+++ Assignment3.ino
@@ -1,153 +1,149 @@
-// Assignment 3 FreeRToS based
+// Assignment 3 by Maksims Latkovskis
+// Performs several functions using the FreeRTOS library (refer the subroutine description)
+
+// Defining the single running core for the RTOS
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
 #else
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
+// Declaring I/O pins
+#define ANALOGPIN 36 // Pin for analog input ADC1_CH0
+#define ERRORPIN 23 // Pin for the error indication LED
 #define WATCHDOGPIN 19 // Pin for the output watchdog waveform 
-#define ANALOGPIN 36 
-#define ANALOGREADPIN 17
 #define DIGITALINPUTPIN 22 // Pin for the pushbutton input
 #define SIGNALPIN 18 // Pin for the external square wave input
-#define ERRORPIN 23 // Pin for the error indication LED
-// Defining global variables
+#define ANALOGREADPIN 17 // Pin to display the analog reading time
 
-unsigned int analogReading; // Very last value to be stored from the ADC
+// Declaring several global variables (protected with semaphores)
+unsigned int analogReading; // Last analog reading to pass between AnalogRead and AnalogAverage
+unsigned int errorCode; // Error code to pass between the AnalogAverage and ErrorCheck
 
- int temp;
- unsigned int errorCode; // value to pass between the analog error detection
+struct memory{ // Defining new data type called "memory"
+  char buttonInput; // State of the button
+  unsigned int measuredFreq; // Measured frequency variable
+  unsigned int analogAverage; // Analog average variable
+  } dataOut; // Create new variable of type "memory" to pass the data to the serial port (protected using binary semaphore)
+
+ int temp; //++++++++++++++++++delete it!
+
 
 //Defining subroutines to be run using RTOS shedulling
-void Watchdog(void *Parameters);
-void AnalogRead(void *Parameters);
-void AnalogAverage(void *Parameters);
-void Nop1000times(void *Parameters);
-void DigitalInput(void *Parameters);
-void FrequencyMeasure(void *Parameters);
-void ErrorCheck(void *Parameters);
-void ErrorDisplay(void *Parameters);
+void Watchdog(void *Parameters); // Subroutine that generates the 50us pulse watchdog
+void AnalogRead(void *Parameters); // Subroutine that reads the analog input pin
+void AnalogAverage(void *Parameters); // Subroutine that computes average analog readings over the last 4 measurements
+void Nop1000times(void *Parameters); // Subroutine that forces CPU to do absolutely nothing 1000 times
+void DigitalInput(void *Parameters); // Subroutine that reads the button state and if pressed outputs stream of data through the serial port
+void FrequencyMeasure(void *Parameters); // Subroutine that measures the frequency of the 50% square signal in the range from 300 Hz up to 10 kHz
+void ErrorCheck(void *Parameters); // Subroutine that evaluates the average analog value and sets appropriate error state
+void ErrorDisplay(void *Parameters); // Subroutine that turns the LED indication on or off, depending on the error state
 
+// Creating the reference parameters of NULL value for the semaphore creation (static prevents compiler from optimization)
 static SemaphoreHandle_t  analogSMP=NULL; 
 static SemaphoreHandle_t  dataOutSMP=NULL; 
 static SemaphoreHandle_t  errorSMP=NULL; 
 
-struct memory{
-  char buttonInput;
-  unsigned int measuredFreq;
-  unsigned int analogAverage;
-} dataOut;
+// Creating the reference parameters of NULL value for the queue creation (static prevents compiler from optimization)
+static QueueHandle_t  analogAvgQUEUE=NULL;
 
-
-
-void setup(void) {
-  // put your setup code here, to run once:
-  Serial.begin (9600);
- pinMode(WATCHDOGPIN, OUTPUT);
-pinMode(ANALOGREADPIN, OUTPUT);
-pinMode(DIGITALINPUTPIN, INPUT);
-pinMode(DIGITALINPUTPIN, INPUT_PULLDOWN); // Prevents pin from being floating 
-pinMode(SIGNALPIN, INPUT);
-pinMode(SIGNALPIN, INPUT_PULLDOWN); // Prevents pin from being floating 
+//==============================================================================================================================================
+void setup(void) 
+{
+  Serial.begin (115200); // Initialize serial communication with the 115200 baud rate
+  pinMode(WATCHDOGPIN, OUTPUT);
+  pinMode(ANALOGREADPIN, OUTPUT);
+  pinMode(DIGITALINPUTPIN, INPUT);
+  pinMode(DIGITALINPUTPIN, INPUT_PULLDOWN); // Prevents pin from being floating 
+  pinMode(SIGNALPIN, INPUT);
+  pinMode(SIGNALPIN, INPUT_PULLDOWN); // Prevents pin from being floating 
   pinMode(ERRORPIN, OUTPUT);
 
-analogSMP = xSemaphoreCreateBinary();
-dataOutSMP = xSemaphoreCreateBinary();
-errorSMP = xSemaphoreCreateBinary();
-
-
-  if(analogSMP==NULL||dataOutSMP==NULL || errorSMP == NULL)
-  {
-    Serial.print("Could not create semaphore");
-    ESP.restart();
-  }
-
-//vTaskStartScheduler(); // maybe it is REQUUIRED BUT I DONT KNOW +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  analogSMP = xSemaphoreCreateBinary(); // Creating binary semaphore to protect analogReading global variable
+  dataOutSMP = xSemaphoreCreateBinary(); // Creating binary semaphore to protect dataOut global variable
+  errorSMP = xSemaphoreCreateBinary(); // Creating binary semaphore to protect errorCode global variable
+  
+  analogAvgQUEUE=xQueueCreate(1,sizeof(unsigned int)); // Creating queue of length 1 to pass the average analog value between tasks
 
   xTaskCreatePinnedToCore(
-    Watchdog
-    ,  "Watchdog"   // A name just for humans
-    ,  570  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+    Watchdog // Generates the 50us pulse watchdog
+    ,  "Watchdog" // Subroutine name for debugging
+    ,  587  // 523 bytes used + 64 bytes (RTOS requirement) = 587 
+    ,  NULL // Subroutine parameter
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
   
   xTaskCreatePinnedToCore(
-    AnalogRead
-    ,  "AnalogRead"   // A name just for humans
-    ,  740  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+    AnalogRead // Reads the analog value from the ANALOGPIN pin
+    ,  "AnalogRead" // Subroutine name for debugging
+    ,  754  // 690 bytes used + 64 bytes (RTOS requirement) = 754 
+    ,  NULL // Subroutine parameter
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-      xTaskCreatePinnedToCore(
-    AnalogAverage
-    ,  "AnalogAverage"   // A name just for humans
-    ,  615  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+  xTaskCreatePinnedToCore(
+    AnalogAverage // Computes average of last four analog readings
+    ,  "AnalogAverage" // Subroutine name for debugging
+    ,  528  // 464 bytes used + 64 bytes (RTOS requirement) = 528 
+    ,  NULL // Subroutine parameter
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-
-        xTaskCreatePinnedToCore(
-    Nop1000times
-    ,  "Nop1000times"   // A name just for humans
-    ,  530  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+  xTaskCreatePinnedToCore(
+    Nop1000times // Sets CPU in no-operation state for 1000 clock cycles
+    ,  "Nop1000times" // Subroutine name for debugging
+    ,  532  // 468 bytes used + 64 bytes (RTOS requirement) = 532 
+    ,  NULL // Subroutine parameter
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-
-            xTaskCreatePinnedToCore(
+  xTaskCreatePinnedToCore(
    DigitalInput
-    ,  "DigitalInput"   // A name just for humans
-    ,  560  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+    ,  "DigitalInput" // Subroutine name for debugging
+    ,  800  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL // Subroutine parameter
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-    
-            xTaskCreatePinnedToCore(
-   FrequencyMeasure
-    ,  "FrequencyMeasure"   // A name just for humans
+  xTaskCreatePinnedToCore(
+   FrequencyMeasure // Measures the frequency of the 50% duty cycle square wave
+    ,  "FrequencyMeasure" // Subroutine name for debugging
     ,  540 // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
+    ,  NULL // Subroutine parameter
     ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-            xTaskCreatePinnedToCore(
-   ErrorCheck
-    ,  "ErrorCheck"   // A name just for humans
-    ,  550 // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+  xTaskCreatePinnedToCore(
+       ErrorCheck // Checks whether the average analog value is greater than the half of the maximum possible analog reading (?>(4096/2)-1)
+    ,  "ErrorCheck"  // Subroutine name for debugging
+    ,  544 // 480 bytes used + 64 bytes (RTOS requirement) = 544 
+    ,  NULL // Subroutine parameter
+    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-     xTaskCreatePinnedToCore(
-   ErrorDisplay
-    ,  "ErrorDisplay"   // A name just for humans
-    ,  530 // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL // task handler
+  xTaskCreatePinnedToCore(
+       ErrorDisplay // Toggles the LED if the error occurs
+    ,  "ErrorDisplay" // Subroutine name for debugging
+    ,  526 // 462 bytes used + 64 bytes (RTOS requirement) = 526 
+    ,  NULL // Subroutine parameter
+    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL // Subroutine handler (not used in this code)
     ,  ARDUINO_RUNNING_CORE);
 
-
-
-//xSemaphoreGive(analogSMP); // Assigning initial state to semaphores
-xSemaphoreGive(dataOutSMP);
-
+  xSemaphoreGive(dataOutSMP); // Makes the semaphore to be initially 1 to operate properly 
 }
+//==============================================================================================================================================
 
+//==============================================================================================================================================
 void loop(void) {} //Not used, since it is RTOS
-
+//==============================================================================================================================================
 
 //====================================================================================================================
 void AnalogRead(void *Parameters) // Reads the analog value from the ANALOGPIN pin
@@ -192,6 +188,7 @@ void AnalogAverage(void *Parameters) // Computes average of last four analog rea
   // This section computes the average of last four readings
   dataOut.analogAverage=analogData[0]+analogData[1]+analogData[2]+analogData[3]; // Sums all values in the array
   dataOut.analogAverage=dataOut.analogAverage>>2; // Divides the sum by four to obtain average
+  xQueueOverwrite(analogAvgQUEUE,&dataOut.analogAverage); // saving average value into the queue
   xSemaphoreGive(dataOutSMP);
    vTaskDelay(40); //42-2 for synronisation
   }
@@ -199,7 +196,7 @@ void AnalogAverage(void *Parameters) // Computes average of last four analog rea
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
-void Nop1000times(void *Parameters) // Sets CPU in no-operation state for 1000 clock cycles (approx. 1ms); two for loops to simplify counter register access
+void Nop1000times(void *Parameters) // Sets CPU in no-operation state for 1000 clock cycles; two for loops to simplify counter register access
 {
   (void) Parameters;
 //heap 468, time neglectably small
@@ -275,12 +272,14 @@ void ErrorCheck(void *Parameters) // Checks whether the average analog value is 
 {
   //600-120+64=546 (550)
   (void) Parameters;
+  unsigned int average;
   while(1){
-  xSemaphoreTake(dataOutSMP,portMAX_DELAY);
-  if(dataOut.analogAverage>2047) {errorCode = 1;} // Setting the error flag 
+ // xSemaphoreTake(dataOutSMP,portMAX_DELAY);
+ xQueueReceive(analogAvgQUEUE,&average,0); // read from queue instantly
+  if(average>2047) {errorCode = 1;} // Setting the error flag 
   else{errorCode=0;} // Clearing the error flag
   xSemaphoreGive(errorSMP); // synchronisation semaphore
-   xSemaphoreGive(dataOutSMP);
+  // xSemaphoreGive(dataOutSMP);
      
    vTaskDelay(333);
   }
@@ -288,13 +287,13 @@ void ErrorCheck(void *Parameters) // Checks whether the average analog value is 
 }
 //----------------------------------------------------------------------------------------------------------------------------------------------
 void ErrorDisplay(void *Parameters) // Toggles the LED if the error occurs
-{ //600-140+64=526 (530)
-  (void) Parameters;
-  while(1){
-    xSemaphoreTake(errorSMP,portMAX_DELAY);
-  digitalWrite(ERRORPIN,errorCode); // Switched the LED either ON or OFF
-     temp=uxTaskGetStackHighWaterMark(nullptr);
-   vTaskDelay(330); //333-3 for syncronisation
+{ 
+  (void) Parameters; // No parameters are being passed to the subroutine
+  while(1) // Run this code forever
+  {
+    xSemaphoreTake(errorSMP,portMAX_DELAY); // Wait infinitely long for the error code to be assigned (synchronisation)
+    digitalWrite(ERRORPIN,errorCode); // Switch the LED either ON or OFF
+    vTaskDelay(330); // Making the repetition period to be 330 ms to allow smooth syncronisation with ErrorCheck subroutine
   }
 }
 //----------------------------------------------------------------------------------------------------------------------------------------------
@@ -373,16 +372,16 @@ measurement:
 }
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
-void Watchdog(void *Parameters) // Generates the 50us pulse
+//----------------------------------------------------------------------------------------------------------------------------------------------
+void Watchdog(void *Parameters) // Subroutine that generates the 50us pulse watchdog
 {
-  (void) Parameters;
-
-   while(1) {
-
-  digitalWrite(WATCHDOGPIN,HIGH);
-  delayMicroseconds(50);
-  digitalWrite(WATCHDOGPIN,LOW);
-
-  vTaskDelay(25);
+   (void) Parameters; // No parameters are being passed to the subroutine
+   while(1) // Run this code forever
+   {
+      digitalWrite(WATCHDOGPIN,HIGH);
+      delayMicroseconds(50);
+      digitalWrite(WATCHDOGPIN,LOW);
+      vTaskDelay(25); // Makes the repetition period of 25 miliseconds (40 Hz)
    }
 }
+//----------------------------------------------------------------------------------------------------------------------------------------------
